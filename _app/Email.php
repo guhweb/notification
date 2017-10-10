3ead95ab875b2cb0dd315966f92fd3951d0c0e7e
Gustavo Web <gustavo@upinside.com.br>
2017-10-10 10:32:43 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -9,21 +9,21 @@ class Email
 {
     private $mail = \stdClass::class;
 
-    public function __construct()
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;                                 // Enable verbose debug output
-        $this->mail->isSMTP();                                      // Set mailer to use SMTP
-        $this->mail->Host = 'mail.gustavoweb.me';  // Specify main and backup SMTP servers
-        $this->mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $this->mail->Username = 'sender@gustavoweb.me';                 // SMTP username
-        $this->mail->Password = 'teste@123';                           // SMTP password
-        $this->mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $this->mail->Port = 587;                                    // TCP port to connect to
+        $this->mail->SMTPDebug = $smtpDebug;                                  // Enable verbose debug output
+        $this->mail->isSMTP();                                                // Set mailer to use SMTP
+        $this->mail->Host = $host;                                            // Specify main and backup SMTP servers
+        $this->mail->SMTPAuth = true;                                         // Enable SMTP authentication
+        $this->mail->Username = $user;                                        // SMTP username
+        $this->mail->Password = $pass;                                        // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                                // Enable TLS encryption, `ssl` also accepted
+        $this->mail->Port = $port;                                            // TCP port to connect to
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('gustavo@gustavoweb.me', 'Equipe GustavoWeb');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName)
--- test/index.php
+++ test/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "mail.gustavoweb.me", "sender@gustavoweb.me", "teste@123", "tls", '587',
+    "gustavo@gustavoweb.me", "Equipe GustavoWeb");
 $novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b>!</p>", "gustavo@gustavoweb.me",
     "Gustavo Web", "gustavo@upinside.com.br", "Gustavo");
 
